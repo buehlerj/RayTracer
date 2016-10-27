7d0f3fc689e956e88648a1b44f1d5c660b96204b
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-10-26 21:56:12 -0600

    pyramid model and ray tracer hitting faces

--- src/RayTracer.java
+++ src/RayTracer.java
@@ -9,6 +9,7 @@ public class RayTracer {
 	private Vertex topRightCorner;
 	private Vertex bottomRightCorner;
 	private Vertex bottomLeftCorner;
+	private ArrayList<Ray> rays = new ArrayList<>();
 
 	public Camera getCamera() {
 		return camera;
@@ -101,7 +102,7 @@ public class RayTracer {
 		double tmax = 0;
 		double distance;
 
-		// Get Distance Specs
+		// Get all Rays
 		for (int j = 0; j < camera.getRes()[1]; j++) {
 			currentLeftHeight = new Vertex(leftOfNCP.getPoint((double) j / camera.getRes()[1]));
 			currentRightHeight = new Vertex(rightOfNCP.getPoint((double) j / camera.getRes()[1]));
@@ -109,13 +110,10 @@ public class RayTracer {
 			for (int i = 0; i < camera.getRes()[0]; i++) {
 				currentPoint = new Vertex(currentVector.getPoint((double) i / camera.getRes()[0]));
 				ray = new Ray(new Vertex(), currentPoint);
-				distance = getDistance(ray);
-				if (distance > tmax)
-					tmax = distance;
-				if (distance < tmin)
-					tmin = distance;
+				rays.add(ray);
 			}
 		}
+		CramersRule();
 
 		// Set Each Pixel's RGB Value
 		double ratio;
@@ -141,47 +139,114 @@ public class RayTracer {
 		}
 	}
 
-	public double getDistance(Ray ray) {
-		double distance = 0;
+	public void CramersRule() {
 		Model currentModel = models.get(0);
+		// Indexed by Face.
+		// a: first vertex (0)
+		// b: second vertex (1)
+		// c: third vertex (2)
+		ArrayList<Vertex> a = new ArrayList<Vertex>();
+		ArrayList<Vertex> b = new ArrayList<Vertex>();
+		ArrayList<Vertex> c = new ArrayList<Vertex>();
 		ArrayList<Matrix> coefficients = new ArrayList<Matrix>();
-		ArrayList<Matrix> xMatrices = new ArrayList<Matrix>();
-		ArrayList<Matrix> yMatrices = new ArrayList<Matrix>();
-		ArrayList<Matrix> zMatrices = new ArrayList<Matrix>();
-		ArrayList<Double> x;
-		ArrayList<Double> y;
-		ArrayList<Double> z;
+		ArrayList<Matrix> betas = new ArrayList<Matrix>();
+		ArrayList<Matrix> gammas = new ArrayList<Matrix>();
+		ArrayList<Matrix> tValues = new ArrayList<Matrix>();
+		Matrix coefficient;
+		Matrix xMatrix;
+		Matrix yMatrix;
+		Matrix zMatrix;
+		double[][] empty = new double[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
 		for (Face f : currentModel.getFaces()) {
-			x = new ArrayList<Double>();
-			y = new ArrayList<Double>();
-			z = new ArrayList<Double>();
+			if (f.getNumberOfVertices() > 3)
+				System.err.println("ERROR: Face has more than 3 vertices");
+			// For all vertices on this face
+			int vertexIndex = 0;
 			for (int i : f.getVertexIndices()) {
 				Vertex currentVertex = currentModel.getVertices().get(i);
-				x.add(currentVertex.getX());
-				y.add(currentVertex.getY());
-				z.add(currentVertex.getZ());
+				if (vertexIndex == 0)
+					a.add(currentVertex);
+				else if (vertexIndex == 1)
+					b.add(currentVertex);
+				else if (vertexIndex == 2)
+					c.add(currentVertex);
+				vertexIndex++;
 			}
-			// a: 0
-			// b: 1
-			// c: 2
-			double a1 = x.get(0) - x.get(1);
-			double a2 = y.get(0) - y.get(1);
-			double a3 = z.get(0) - z.get(1);
-			double b1 = x.get(0) - x.get(2);
-			double b2 = y.get(0) - y.get(2);
-			double b3 = z.get(0) - z.get(2);
-			double[][] coefficientValues = new double[][] { { a1, b1, 0 }, { a2, b2, 0 }, { a3, b3, 0 } };
-			coefficients.add(new Matrix(coefficientValues));
-			double[][] xValues = new double[][] { { 0, b1, 0 }, { 0, b2, 0 }, { 0, b3, 0 } };
-			xMatrices.add(new Matrix(xValues));
-			double[][] yValues = new double[][] { { a1, 0, 0 }, { a2, 0, 0 }, { a3, 0, 0 } };
-			yMatrices.add(new Matrix(yValues));
-			double[][] zValues = new double[][] { { a1, b1, 0 }, { a2, b2, 0 }, { a3, b3, 0 } };
-			zMatrices.add(new Matrix(zValues));
 		}
 
-		return distance;
+		int rayIndex = 0;
+		boolean hitFace = false;
+		for (Ray r : rays) {
+			hitFace = false;
+			int faceIndex = 0;
+			for (Face f : currentModel.getFaces()) {
+				if (hitFace == true)
+					break;
+				double a1 = a.get(faceIndex).getX() - b.get(faceIndex).getX();
+				double a2 = a.get(faceIndex).getY() - b.get(faceIndex).getY();
+				double a3 = a.get(faceIndex).getZ() - b.get(faceIndex).getZ();
+				double b1 = a.get(faceIndex).getX() - c.get(faceIndex).getX();
+				double b2 = a.get(faceIndex).getY() - c.get(faceIndex).getY();
+				double b3 = a.get(faceIndex).getZ() - c.get(faceIndex).getZ();
+				double c1 = r.getD().getX();
+				double c2 = r.getD().getY();
+				double c3 = r.getD().getZ();
+				double[][] coefficientValues = new double[][]{
+					{a1, b1, c1}, {a2, b2, c2}, {a3, b3, c3}
+				};
+				double[][] xValues = new double[][]{
+					{a.get(faceIndex).getX() - r.getOrigin().getX(), b1, c1},
+					{a.get(faceIndex).getY() - r.getOrigin().getY(), b2, c2},
+					{a.get(faceIndex).getZ() - r.getOrigin().getZ(), b3, c3}
+				};
+				double[][] yValues = new double[][]{
+					{a1, a.get(faceIndex).getX() - r.getOrigin().getX(), c1},
+					{a2, a.get(faceIndex).getY() - r.getOrigin().getY(), c2},
+					{a3, a.get(faceIndex).getZ() - r.getOrigin().getZ(), c3}
+				};
+				double[][] zValues = new double[][]{
+					{a1, b1, a.get(faceIndex).getX() - r.getOrigin().getX()},
+					{a2, b2, a.get(faceIndex).getY() - r.getOrigin().getY()},
+					{a3, b3, a.get(faceIndex).getZ() - r.getOrigin().getZ()}
+				};
+				coefficient = new Matrix(coefficientValues);
+				xMatrix = new Matrix(xValues);
+				yMatrix = new Matrix(yValues);
+				zMatrix = new Matrix(zValues);
+				double determinatnCoefficient = coefficient.det();
+				double beta = xMatrix.det() / determinatnCoefficient;
+				double gamma = yMatrix.det() / determinatnCoefficient;
+				double t = zMatrix.det() / determinatnCoefficient;
+//				System.out.println(beta + ":" + gamma + ":" + t);
+				if (beta >= 0 && gamma >= 0 && (beta + gamma) >= 1 && t > 0) {
+					coefficients.add(coefficient);
+					betas.add(xMatrix);
+					gammas.add(yMatrix);
+					tValues.add(zMatrix);
+					hitFace = true;
+					break;
+				}
+				faceIndex++;
+			}
+			if (hitFace == false) {
+				coefficients.add(new Matrix(empty));
+				betas.add(new Matrix(empty));
+				gammas.add(new Matrix(empty));
+				tValues.add(new Matrix(empty));
+			}
+			rayIndex++;
+		}
+		System.out.println("Resolution: " + camera.getRes()[0] + ", " + camera.getRes()[1]);
+		System.out.println("Rays: " + rays.size());
+		System.out.println("Coefficients: " + coefficients.size());
+		System.out.println("Betas: " + betas.size());
+		System.out.println("Gammas: " + gammas.size());
+		System.out.println("tValues: " + tValues.size() + "\n\n\n");
+	}
+
+	public double getDistance(Ray ray) {
+		return 0;
 	}
 
 	public void determinant() {
