4fa63aa63e155e83afb4abb53b6aab922d1aa74b
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-11-13 15:48:51 -0700

    Main Class handles past assignments

---
+++ src/CS410BuehlerJeff.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class CS410BuehlerJeff {
+
+	public static void main(String[] args) {
+		PA4(args);
+	}
+
+	public static void PA4(String[] args) {
+		RayTracer rayTracer = new RayTracer();
+		if (args.length < 3) {
+			System.err.println("Incorrect number of argments: " + args.length);
+			System.exit(-1);
+		}
+
+		if (!rayTracer.getCamera().read(args[0])) {
+			System.exit(-1);
+		}
+
+		for (String model_file_name : Arrays.copyOfRange(args, 1, args.length - 1)) {
+			Model model = new Model();
+			if (!model.read(model_file_name)) {
+				System.exit(-1);
+			}
+			rayTracer.addModel(model);
+		}
+		System.out.println(rayTracer.getCamera());
+		System.out.println("Wrote to: " + args[args.length - 1]);
+		System.out.println("\n\n\n\n----------------- EXIT -----------------");
+	}
+
+	public static void PA3(String[] args) {
+		RayTracer rayTracer = new RayTracer();
+		if (args.length < 3) {
+			System.err.println("Incorrect number of argments: " + args.length);
+			System.exit(-1);
+		}
+
+		if (!rayTracer.getCamera().read(args[0])) {
+			System.exit(-1);
+		}
+
+		for (String model_file_name : Arrays.copyOfRange(args, 1, args.length - 1)) {
+			Model model = new Model();
+			if (!model.read(model_file_name)) {
+				System.exit(-1);
+			}
+			rayTracer.addModel(model);
+		}
+
+		Picture picture = rayTracer.capturePicture();
+		picture.write(args[args.length - 1], "");
+		System.out.println(rayTracer.getCamera());
+		System.out.println("Wrote to: " + args[args.length - 1]);
+		System.out.println("\n\n\n\n----------------- EXIT -----------------");
+	}
+}
